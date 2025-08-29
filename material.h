e87a29ae2fa28d99ec88d460eb627d06c8aac270
Ruhai_Qiu <qrh818272@gmail.com>
2025-08-29 16:06:28 +0800

    添加了light

--- material.h
+++ material.h
@@ -11,6 +11,10 @@ class material {
 public:
     virtual ~material() = default;
 
+    virtual color emitted(double u, double v, const point3& p) const {
+        return color(0, 0, 0);
+    }
+
     virtual bool scatter(const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered) const {
         return false;
     }
@@ -89,4 +93,18 @@ private:
         return r0 + (1 - r0) * std::pow((1 - cosine), 5);
     }
 };
+
+class diffuse_light : public material {
+public:
+    diffuse_light(shared_ptr<texture> tex) : tex(tex) {}
+    diffuse_light(const color& emit) : tex(make_shared<solid_color>(emit)) {}
+
+    color emitted(double u, double v, const point3& p) const override {
+        return tex->value(u, v, p);
+    }
+
+private:
+    shared_ptr<texture> tex;
+};
+
 #endif //MATERIAL_H
