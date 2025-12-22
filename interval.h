04be892db48c0f1fea784ba4cf893bde18ca7a5c
Ruhai_Qiu <qrh818272@gmail.com>
2025-12-22 11:42:23 +0800

    Ray Tracing in One Weekend Project

---
+++ interval.h
@@ -0,0 +1,54 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+class interval {
+public:
+    double min, max;
+
+    interval() : min(-infinity), max(infinity) {}
+
+    interval(double tmin, double tmax) : min(tmin), max(tmax) {}
+
+    interval(const interval& a, const interval& b) {
+        min = a.min <= b.min ? a.min : b.min;
+        max = a.max >= b.max ? a.max : b.max;
+    }
+
+    double size() const {
+        return max - min;
+    }
+
+    bool contains(double x) const {
+        return x >= min && x <= max;
+    }
+
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
+
+    double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
+    }
+
+    interval expand(double delta) const {
+        auto padding = delta / 2;
+        return interval(min - padding, max + padding);
+    }
+
+    static const interval empty, universe;
+};
+
+const interval interval::empty(+infinity, -infinity);
+const interval interval::universe(-infinity,+infinity);
+
+interval operator+(const interval& ival, double displacement) {
+    return interval(ival.min + displacement, ival.max + displacement);
+}
+
+interval operator+(double displacement, const interval& ival) {
+    return ival + displacement;
+}
+
+#endif //INTERVAL_H
